5225fa3ba72d400f73898e9a9e4455d990dda879
Aaron Hill <Finch659@gmail.com>
2015-05-07 19:02:33 -0400

    Adding first part of chapter four exercises

---
+++ chap4/chap4.js
@@ -0,0 +1,43 @@
+//The sum of a Range
+
+var range = function(start, end, inc) {
+	if (start < end) {
+			var foo = [];
+			if(inc === undefined || isNaN(inc)) {
+				for (var i = start; i <= end; i++) {
+					foo.push(i);
+				}
+			} else {
+				for (var i = start; i <= end; i += inc) {
+					foo.push(i);
+				}
+			}
+		} else if (end < start) {
+		var foo = [];
+			if(inc === undefined || isNaN(inc)) {
+				for (var i = start; i >= end; i--) {
+					foo.push(i);
+				}
+			} else {
+				for (var i = start; i >= end; i -= inc) {
+					foo.push(i);
+				}
+		}
+		}
+	  return foo;
+}
+
+
+var sum = function(bar) {
+	var tote = 0;
+	for(var i = 0; i < bar.length; i++) {
+		tote += bar[i];
+	}
+return tote;
+}
+
+
+
+//Reversing an Array
+
+
